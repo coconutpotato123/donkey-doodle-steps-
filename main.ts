2b26ca281e0e25856187891509f60f92f481a1d1
coconutpotato123 <73363585+coconutpotato123@users.noreply.github.com>
2022-05-12 14:28:30 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+let donkeyImg: Sprite = null
+donkeyImg.setImage(img`
+    . b b b b b b . . . . . . . . . 
+    b b b b b b b b 3 b b . . . . . 
+    b b b b b b b 3 3 3 3 b . . . . 
+    3 b b b b b b b b b b b . . . . 
+    3 b b b b b b b b . . . . . . c 
+    b b b b b b b b . . . . . . c c 
+    . . b b b b b . . . b . . . b . 
+    . . . . b b b b b b b b . b b . 
+    . . . b b b b b b b b b b b . . 
+    . . . b b b b b b b b b b . . . 
+    . . . b b b b b b b b b b . . . 
+    . . . b b b b b b b b b b . . . 
+    . . . b b b b b b b b b b . . . 
+    . . . b b b b b b b b b b b . . 
+    . . b b b b . . . . . b 3 b . . 
+    . . b 3 b . . . . . . b 3 b . . 
+    `)
